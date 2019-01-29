3669f1fe9edca2d6cb1c5c2f1f484c2a178b2b8f
Negi <HNegi@spherasolutions.com>
2019-01-29 17:05:19 +0530

    Updates

---
+++ precache-manifest.bd2728b8e37a5f78f5e94905a6e21a72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1efc6b1d583e0ef872b89972a0b7ee33",
+    "url": "/todolist/static/media/background.1efc6b1d.jpg"
+  },
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "ba8b6d5e09602da997e6",
+    "url": "/todolist/static/js/main.ba8b6d5e.chunk.js"
+  },
+  {
+    "revision": "35d4967322379881d30c",
+    "url": "/todolist/static/js/1.35d49673.chunk.js"
+  },
+  {
+    "revision": "ba8b6d5e09602da997e6",
+    "url": "/todolist/static/css/main.d313ac5c.chunk.css"
+  },
+  {
+    "revision": "096ec3ad32b6c19c9a6e86757bab1b11",
+    "url": "/todolist/index.html"
+  }
+];
